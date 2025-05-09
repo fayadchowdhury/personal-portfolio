89b9152a64309dbc17912e164cb444d541b3ee90
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-05-09 00:10:37 -0700

    Add basic scaffolding. Will need to populate with data and structure into components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,63 @@
 function App() {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md text-center">
-      <h2 className="text-2xl font-bold text-gray-800">HELLO!</h2>
-      <p className="text-gray-600 mt-3">
-        We are in the process of integrating Tailwind and getting things set up
+    // Navbar
+
+    // Router??
+
+    // Main page
+    <div>
+      <h1>Name</h1>
+      <h3>Tagline line 1</h3>
+      <h3>Tagline line 2</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
+        commodi atque voluptatum reiciendis rerum aut molestiae, ratione et modi
+        ducimus ut voluptatibus autem aperiam quia ipsum consequatur est
+        temporibus. Odio?
       </p>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
-        Shiny button!
-      </button>
+
+      <button>Button 1</button>
+      <button>Button 2</button>
+
+      <h1>Work</h1>
+      <ul>
+        <li>Work 1</li>
+        <li>Work 2</li>
+        <li>Work 3</li>
+      </ul>
+
+      <h1>Projects</h1>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+        <li>Project 4</li>
+      </ul>
+
+      <h1>Skills</h1>
+      <ul>
+        <li>Skill 1</li>
+        <li>Skill 2</li>
+        <li>Skill 3</li>
+      </ul>
+
+      <h1>Thoughts</h1>
+      <ul>
+        <li>Thought 1</li>
+        <li>Thought 2</li>
+      </ul>
+
+      <h1>Testimonials</h1>
+      <ul>
+        <li>Testimonial 1</li>
+        <li>Testimonial 2</li>
+        <li>Testimonial 3</li>
+      </ul>
+
+      <h1>Contact</h1>
     </div>
+
+    // Footer
   );
 }
 
