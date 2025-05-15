d053a0f383001b2bcb612234755582b9b4027bb3
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-05-15 01:40:28 -0700

    Create reusable IconButton element

---
+++ src/elements/IconButton.tsx
@@ -0,0 +1,25 @@
+import { ElementType } from "react";
+
+interface IconButtonProps {
+  buttonText: string;
+  Icon?: ElementType;
+  onButtonClick: () => void;
+}
+
+const IconButton = ({ buttonText, Icon, onButtonClick }: IconButtonProps) => {
+  return (
+    <button
+      className="flex grid-cols-2 py-2 px-4 rounded-lg gap-2 border-1 border-white hover:bg-white  text-white hover:text-black"
+      onClick={onButtonClick}
+    >
+      <span className="text-content">{buttonText}</span>
+      {Icon && (
+        <div className="flex items-center justify-center">
+          <Icon className="w-6 h-6 max-sm:w-4 max-sm:h-4" />
+        </div>
+      )}
+    </button>
+  );
+};
+
+export default IconButton;
