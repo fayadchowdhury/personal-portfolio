4043c6583e9969842d41d0418109c7d13b1bc75e
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-08-30 01:08:01 -0700

    Separate dev and prod data

---
+++ src/data/index.ts
@@ -0,0 +1,15 @@
+import data_dev from "./data-dev.json";
+import data_prod from "./data-prod.json"
+
+const mode: string = process.env.ENV || "dev-data";
+let data: typeof data_dev | typeof data_prod;
+
+if (mode === "dev-json") {
+    data = data_dev;
+} else if (mode === "prod-json") {
+    data = data_prod;
+} else {
+    data = data_dev;
+}
+
+export default data;
