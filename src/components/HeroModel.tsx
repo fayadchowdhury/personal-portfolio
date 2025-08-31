17287ea8eeef8e15fdc04a14d51d31bbbc0270af
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-08-31 16:46:06 -0700

    Remove unnecessary import

--- src/components/HeroModel.tsx
+++ src/components/HeroModel.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Bounds } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
