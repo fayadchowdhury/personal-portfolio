64c632b9a8f9244c1548b0dd24b2b9b87e16ac0f
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-09-02 13:39:47 -0700

    Refactor IconButton component into IconAnchor since they do not exhibit button behaviour anyway

---
+++ src/components/IconAnchor.tsx
@@ -0,0 +1,25 @@
+import { ComponentType } from "react";
+
+interface IconAnchorProps {
+  buttonText: string;
+  Icon?: ComponentType<React.SVGProps<SVGSVGElement>>;
+  href?: string;
+}
+
+const IconAnchor = ({ buttonText, Icon, href }: IconAnchorProps) => {
+  return (
+    <a
+      className="flex grid-cols-2 py-2 px-4 rounded-lg gap-2 border-1 border-black dark:border-white hover:bg-black dark:hover:bg-white text-black dark:text-white hover:text-white dark:hover:text-black"
+      href={href}
+    >
+      <span className="text-content">{buttonText}</span>
+      {Icon && (
+        <div className="flex items-center justify-center">
+          <Icon className="w-6 h-6 max-sm:w-4 max-sm:h-4" />
+        </div>
+      )}
+    </a>
+  );
+};
+
+export default IconAnchor;
