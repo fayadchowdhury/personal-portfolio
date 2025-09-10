aae673650447a01e2ab74e952f1561a308a776f2
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-09-10 00:54:40 -0700

    Refactor utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
-export * from "./markdown";
+export * from "./markdownUtils";
+export * from "./stringUtils";
---
+++ src/utils/markdownUtils.ts
@@ -0,0 +1,37 @@
+import removeMarkdown from "remove-markdown";
+import { marked } from "marked";
+
+const markdownToHtmlString = async function (md: string) : Promise<string> {
+  return await marked.parse(md);
+}
+
+const markdownToPlainTextString = function (md: string) : string {
+  return removeMarkdown(md, {
+    stripListLeaders: true,
+    listUnicodeChar: "",
+    gfm: true,
+    useImgAltText: false
+  });
+}
+
+export const extractTldr = function (readme: string): string[] {
+  const tokens = marked.lexer(readme);
+  const features: string[] = [];
+  let insideFeaturesSection: boolean = false;
+
+  for (const token of tokens) {
+    if (token.type === "heading" && token.depth === 3) {
+      insideFeaturesSection = token.text.toLowerCase() === "tl;dr";
+    } else if (insideFeaturesSection && token.type === "list") {
+      for (const item of token.items) {
+        features.push(item.raw);
+      }
+      break;
+    }
+  }
+  return features;
+}
+
+export function readmeToPlainText(readme: string) {
+  return markdownToPlainTextString(readme);
+}
