f0e7e318a5f2930e6187ce6d0cc52c407c43e9fc
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-09-11 14:34:25 -0700

    Sort work cards by order

--- src/sections/Work.tsx
+++ src/sections/Work.tsx
@@ -2,6 +2,7 @@ import ExperienceDescriptionCard from "../components/ExperienceDescriptionCard";
 
 interface WorkProps {
   workItems: {
+    order: number;
     iconPath: string;
     title: string;
     subtitle: string;
@@ -12,15 +13,18 @@ interface WorkProps {
 }
 
 const Work = ({ workItems }: WorkProps) => {
+  console.log(workItems);
   return (
     <section id="work">
       {/* <div className="grid grid-cols-1 md:grid-cols-5"> */}
-      {workItems.map((item, index) => (
-        <div key={index} className="grid grid-cols-1 md:grid-cols-5">
-          <div className="md:col-span-1 hidden md:block"></div>
-          <ExperienceDescriptionCard {...item} />
-        </div>
-      ))}
+      {workItems
+        .sort((a, b) => a.order - b.order)
+        .map((item, index) => (
+          <div key={index} className="grid grid-cols-1 md:grid-cols-5">
+            <div className="md:col-span-1 hidden md:block"></div>
+            <ExperienceDescriptionCard {...item} />
+          </div>
+        ))}
     </section>
   );
 };
