e60741a4f9edd01dcc12915412da6cb4b38659bf
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-09-10 19:02:43 -0700

    Add CTA to take to Github project in modal

--- src/components/ProjectModal.tsx
+++ src/components/ProjectModal.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { readmeToHtml } from "../utils";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import IconAnchor from "./IconAnchor";
 
 interface ProjectModalProps {
   picPath?: string;
@@ -20,9 +22,14 @@ const ProjectModal = ({ picPath, title, url, readme }: ProjectModalProps) => {
   return (
     <div className="modal">
       {picPath && <img className="mb-2" src={picPath} />}
-      <a href={url}>
-        <h1 className="project-modal-text-header">{title}</h1>
-      </a>
+      <h1 className="project-modal-text-header">{title}</h1>
+      <div className="flex items-center max-md:justify-items-center my-2">
+        <IconAnchor
+          buttonText="Check this out on Github!"
+          href={url}
+          Icon={ArrowRightIcon}
+        />
+      </div>
       <div
         className="project-modal-text-content"
         dangerouslySetInnerHTML={{ __html: readmeText }}
